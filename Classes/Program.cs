67fd78f901c45b7c2f010811bd5e84ca0ddcf0b0
Супрун Сергей <4everyongg@mail.ru>
2021-10-23 21:17:34 +0400

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    abstract class A
+    {
+        public string Prop1 { get; set; } = "aaa";
+        public uint Prop2 {get => uint.Parse(Console.ReadLine()); 
+            set 
+            {
+                if (value > 100)
+                {
+                    Math.Truncate((float) value / (10 * (value.ToString().Length - 2)));
+                }
+            } 
+        }
+        /*public int Foo(DateTime time)
+        {
+            return (int)time.Second;
+        } два метода с одинаковыми принимаемыми значениями, компилятор ругается
+*/
+        public abstract void Foo(DateTime time);
+    }
+
+    class B : A
+    {
+        private float PropB1 { get; set; }
+        public override void Foo(DateTime time)
+        {
+            
+        }
+    }
+
+    abstract class C : A
+    {
+        protected Guid PropC1 { get; set; }
+    }
+
+    class D : C
+    {
+        public E PropD1 { get; set; }
+        public override void Foo(DateTime time)
+        {
+
+        }
+    }
+
+    class E
+    {
+        public E PropE1 { get; set; }
+        public void Bar()
+        {
+
+        }
+        private void Bar(int size)
+        {
+
+        }
+    }
+}
